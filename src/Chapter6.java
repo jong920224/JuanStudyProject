e729ecca57538e90e16c8af731efa70f742549d5
@jong920224 <jong920224@naver.com>
2020-05-10 22:06:05 +0900

    2020-05-10
    2020-05-10 커밋

--- src/Chapter6.java
+++ src/Chapter6.java
@@ -1,5 +1,7 @@
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,30 +15,34 @@ import javax.servlet.http.HttpServletResponse;
 public class Chapter6 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Chapter6() {
         super();
-        System.out.println("init");
-		// TODO Auto-generated constructor stub
-		// 2020 Test
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// 화면 단에서 던진 파라미터
+		String param = (String) request.getParameter("param");
+		
+		// WEB-INF 폴더안에 있는 html, jsp파일은 직접적으로 접근이 불가능하기 떄문에
+		// 서버에서 화면을 호출해야 함. 
+		String testURL = "/WEB-INF/" + param + "/chapter6_view/chapter6.html";
+		
+		// 위에 testURL의 경로로 이동하기 위해
+		// Dispatcher forward 방식을 이용함.
+		RequestDispatcher dispatcher = null;
+		dispatcher = request.getRequestDispatcher(testURL);
+		
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println(request.getHeaderNames());
+		System.out.println(request.getContentType());
+		doGet(request, response);	
 	}
 
 }
