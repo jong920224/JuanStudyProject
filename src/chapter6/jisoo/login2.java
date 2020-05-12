27db8ab47625eef6763bd06159b9d86d452629ec
@jong920224 <jong920224@naver.com>
2020-05-12 22:04:16 +0900

    2020-05-12 Commit
    2020-05-12 Commit

--- src/chapter6/jisoo/login2.java
+++ src/chapter6/jisoo/login2.java
@@ -1,6 +1,8 @@
 package chapter6.jisoo;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,36 +10,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class login
+ * Servlet implementation class login2
  */
-@WebServlet("/login")
-public class login extends HttpServlet {
+@WebServlet("/login2")
+public class login2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public login() {
+    public login2() {
         super();
     }
-
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		// HttpServletRequest
-		// 웹 브라우저에서 전송한 정보를 톰캣 컨테이너가 HttpServletRequest 객체를 생성한 후 doGet으로 넘겨줌
-		
-		// 전송 될 데이터를 UTF-8로 인코딩
 		request.setCharacterEncoding("UTF-8");
 		
-		// getParameter()을 이용하여 input 태그의 name 속성 값으로 전송된 value를 받음.
-		String user_id = request.getParameter("user_id");
-		String user_pw = request.getParameter("user_pw");
+		// 응답 할 데이터 종류가 html임을 명시
+		response.setContentType("text/html; charset=utf-8");
 		
-		System.out.println(user_id);
-		System.out.println(user_pw);
+		// 출력용도
+		PrintWriter out = response.getWriter();
 		
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + id;
+		data += "<br>";
+		data += "비밀번호 : " + pw;
+		data += "</body>";	
+		data += "</html>";	
+		
+		out.print(data);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
 	}
 
 }
